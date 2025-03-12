8050d210e87999955807abf00ea78e8760d141fa
강현우 <kangs97@naver.com>
2025-03-12 17:41:37 +0900

    main2 수정중

---
+++ Together/src/main/java/edu/kh/project/individual/service/RecruitmentService.java
@@ -0,0 +1,18 @@
+package edu.kh.project.individual.service;
+
+import java.util.List;
+
+import edu.kh.project.individual.dto.Recruitment;
+
+public interface RecruitmentService {
+
+	// 공동구매 모집글 조회 (BOARD_CD = 1인 글만) ,최신글
+    List<Recruitment> selectRecruitmentList(int boardCode, int memberNo);
+
+	// 모집방 상세 조회
+	Recruitment selectRecruitmentDetail(int recruitmentNo);
+
+	// 조회순 글
+	List<Recruitment> selectRecruitmentListByViewCount(int boardCode, int memberNo);
+
+}
