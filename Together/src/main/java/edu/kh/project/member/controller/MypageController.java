67e55d41a0b491ee508594e18bdf9f3e6b99afac
박천상 <1998pcs@gmail.com>
2025-03-28 20:06:01 +0900

    MypageController에서 Map 임포트 추가

--- Together/src/main/java/edu/kh/project/member/controller/MypageController.java
+++ Together/src/main/java/edu/kh/project/member/controller/MypageController.java
@@ -22,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
